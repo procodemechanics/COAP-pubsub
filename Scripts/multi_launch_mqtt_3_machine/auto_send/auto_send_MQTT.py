ad96793b9b803014374a73364a2542f09fe684bd
Aman Gulia <gulia@kth.se>
2018-10-06 20:26:36 +0200

    Publish message scripts to run on localhost and network

---
+++ Scripts/multi_launch_mqtt_3_machine/auto_send/auto_send_MQTT.py
@@ -0,0 +1,24 @@
+"""Publish MQTT packets by specifying the number of transactions in the argument """
+
+import os
+import sys
+import subprocess
+
+#Snif on port 1883 corresponding to mqtt port
+#process = subprocess.Popen(["tcpdump -i lo -w ~/IoTresults/result.pcap 'port 1883'"], shell=True)
+
+#Create a subprocess to publish mqtt messages
+mqtt_load= subprocess.Popen('mosquitto_sub -t test', shell=True)
+for i in range(int(sys.argv[1])):
+	os.system('mosquitto_pub -m "message index ..."'+ str(i) +' -t test -q 2 -d')
+mqtt_load.terminate()
+pid_mqtt_pub = mqtt_load.pid
+os.system('sudo kill -9 '+str(pid_mqtt_pub))
+
+#Snif on port 1883 corresponding to mqtt port
+process = subprocess.Popen(["tcpdump -i lo -w ~/IoTresults/result.pcap 'port 1883'"], shell=True)
+
+process.terminate()
+
+
+
