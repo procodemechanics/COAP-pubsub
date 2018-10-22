ee83c21ea3adefbbd2d96a874c1f7b94d53286d2
Jerome de Chauveron <jeromedc@kth.se>
2018-10-22 16:00:06 +0200

    Update on bash launch file

--- Scripts/multi_launch_mqtt_3_machine/launch.sh
+++ Scripts/multi_launch_mqtt_3_machine/launch.sh
@@ -1,26 +1,23 @@
 touch result_loss
 touch result_delay
-sudo tc qdisc add dev $5 netem loss 0.001%
-sudo tc qdisc add dev $5 netem delay 0.0ms
+sudo tc qdisc add dev $5 root netem loss 0.001%
+sudo tc qdisc add dev $5 root netem delay 0.0ms
 
 if [ "$1" == "loss" ]
 then
-	for loss_value in  0.1 0.5 1 1.5 2.0 5.0 7.5 10.0 15.0 25.0 35.0 45.0 
+	for loss_value in  1  5.0 7.5 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 
 	do
-		sudo tc qdisc change dev $5 netem loss $loss_value%
+		sudo tc qdisc change dev $5 root netem loss $loss_value%
 		sudo python launcher.py send-and-print $2 $3 $4 $5 $6 $7 $8 $loss_value 0 result_loss
 	done
 	gnuplot printing_delay.gp
 fi
-if [ "$1" == "loss" ]
+if [ "$1" == "delay" ]
 then
-	for delay_value in  1.0 2.0 5.0 10.0  20.0 30.0 40.0 50.0 60.0 70.0 80.0 90.0 100.0
+	for delay_value in  0.5 1.0 2.0 5.0 7.5 10.0 12.5 15.0 
 	do
-		sudo tc qdisc change dev $5 netem delay $delay_value ms
+		sudo tc qdisc change dev $5 root netem delay $delay_valuems
 		sudo python launcher.py send-and-print $2 $3 $4 $5 $6 $7 $8 0.0 $delay_value result_delay
 	done
 	gnuplot printing_loss.gp
 fi 
-
-sudo rm result_loss
-sudo rm result_delay
