a33ea7e6394820febb9b3ea2b837d81578f1f553
Jerome de Chauveron <jeromedc@kth.se>
2018-10-23 16:30:19 +0200

    Update on MQTT scripting for loss before midterm

--- Scripts/multi_launch_mqtt_3_machine/launch.sh
+++ Scripts/multi_launch_mqtt_3_machine/launch.sh
@@ -5,19 +5,21 @@ sudo tc qdisc add dev $5 root netem delay 0.0ms
 
 if [ "$1" == "loss" ]
 then
-	for loss_value in  1  5.0 7.5 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 
+	for loss_value in  1.0 2.5 5.0 7.5 10 12.5 15 17.5 20 22.5 25 27.5 30 
 	do
 		sudo tc qdisc change dev $5 root netem loss $loss_value%
 		sudo python launcher.py send-and-print $2 $3 $4 $5 $6 $7 $8 $loss_value 0 result_loss
 	done
+	sleep 1
 	gnuplot printing_delay.gp
 fi
 if [ "$1" == "delay" ]
 then
-	for delay_value in  0.5 1.0 2.0 5.0 7.5 10.0 12.5 15.0 
+	for delay_value in 1.0 10.0 50.0 100.0 500.0 1000.0 5000.0 10000.0
 	do
 		sudo tc qdisc change dev $5 root netem delay $delay_valuems
 		sudo python launcher.py send-and-print $2 $3 $4 $5 $6 $7 $8 0.0 $delay_value result_delay
 	done
+	sleep 1
 	gnuplot printing_loss.gp
 fi 
--- Scripts/multi_launch_mqtt_3_machine/printing_loss.gp
+++ Scripts/multi_launch_mqtt_3_machine/printing_loss.gp
@@ -1,6 +1,8 @@
 set terminal pdf
 set output "loss_graph.pdf"
 set xlabel "Loss %"
-set ylabel "RTT ms"
+set ylabel "Average RTT ms"
 set title "RTT in function of the loss"
+set xrange [3:17]
+set yrange [0:100]
 plot "result_loss" using 1:($3*1000):4 with yerrorbars title 'RTT'
