27dfaab55760a0247ffc330ed9d57fd11a7b60fb
Aman Gulia <gulia@kth.se>
2018-11-08 18:19:21 +0100

    This script can be used to open a subscriber connection and a publisher connection. The publisher part allows to specify the number of messages to be published to the broker.

---
+++ Scripts/m2m_coap/m2m-mqtt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+case $1 in
+	mqtt_sub)
+	mosquitto_sub -h $2 -t m2m-mqtt &
+	;;
+	mqtt_pub)
+	if [ $(($3)) != 0 ]; then
+                for ((c=1; c<=$(($3)); c++))
+                do
+			mosquitto_pub -h $2 -t m2m-mqtt -m 'Test msg = '$(($c))''
+		done
+	fi
+	;;
+	-h)
+	echo "############################ Help #############################"
+	echo "This script is for sending multiple MQTT publish messages to"
+	echo "1. MSG TYPE(mqtt_sub or mqtt_pub) "
+	echo "2. BROKER IP ADDRESS "
+	echo "3. Number of messages to be published "
+	echo "bash m2m-mqtt.sh [mqtt_pub] [broker_ip] [number of msg] "
+	echo "bash m2m-mqtt.sh [mqtt_sub] [broker_ip]"
+	echo " "
+	echo "Example: bash m2m-mqtt.sh mqtt_sub 127.0.0.1"
+	echo "Example: bash m2m-mqtt.sh mqtt_pub 127.0.0.1 1"
+	echo "################################################################"
+
+	;;
+esac
