4dc6f0f84c5b824e4e27b9d645e9bc7250767358
Xenon <pong@kth.se>
2018-11-23 05:28:05 -0800

    Update iot-loop.sh and netem_ssh.sh. netem_ssh.sh has more function for add, delete, burst loss, random loss, and delay. in iot-loop.sh has function of run the initial capture without netem for both protocol and update the code for mqtt. iot-loop version 2

--- Scripts/iot-loop/iot-loop.sh
+++ Scripts/iot-loop/iot-loop.sh
@@ -15,14 +15,14 @@
 #Start#
 case $8 in
 	mqtt )
-	sshpass -p "howislife" ssh -t student@192.168.1.2 "mkdir TEST && cd TEST"
+	sshpass -p "howislife" ssh -t student@192.168.1.2 "mkdir TEST"
 ;;
 esac
 case $1 in
 	-h)
 
 	############### HELP ####################################
-	echo "bash iot-loop.sh [interface/put/-h] [coap/mqtt] [output] [ip] [no of trial loop][total packet]"
+	echo "bash iot-loop.sh"
 	echo ""	
 	echo "Parameter list:"	
 	echo ""	
@@ -42,6 +42,7 @@ case $1 in
 	echo "9. QoS"
 	echo "10 MQTT sub IP"
 	echo "############### Example #########################"
+	echo "bash iot-loop.sh put 192.168.1.2 Run "
 	echo "bash iot-loop.sh enp2s0 test-coap 5683 192.168.1.2 2 100 ~/TEST-RUN/iot-loop coap"
 	echo "bash iot-loop.sh enp2s0 test-mqtt 1883 192.168.1.2 2 100 ~/TEST-RUN/iot-loop mqtt 1"
 	echo "#########################################################"	
@@ -51,8 +52,6 @@ case $1 in
 	#########################################################
 	;;
 
-
-
 	put)
 
 	########## This is just for putting the data into the server ###############
@@ -60,42 +59,207 @@ case $1 in
 	;;
 
 	#################### Else ####################################
+
+	init)
+		### Parameter
+		### $2 => Name
+		### $3 => coap/mqtt
+		### $4 => maximun message
+		### $5 => IP
+		### $6 => interface
+		### $7 => folder of script
+	echo "Start"
+	fol=$2
+	mkdir $fol
+	cd $fol
+
+	case $3 in
+		coap)
+		
+
+
+		echo "###### Initial phase start 1-$4 ###"
+		fol2="$fol-init"
+		mkdir $fol2
+		cd $fol2
+		############ Make sure it Remove NetEM #############
+		bash "$7/netem_ssh.sh" add
+		bash "$7/netem_ssh.sh" del 
+		# bash "$7/netem_ssh.sh" show
+		############### Start #################
+		for ((c=1; c<=$4;c++))
+		do
+
+		############## Start the process ##############
+		echo " ############# Round $c ###########"
+		############## Start Capture  #################
+		sudo tcpdump -i $6 -w $2-init-$c.pcap 'port 5683' &
+		sleep 2
+		echo "########### Start ################"
+		#    ~/m2m-coap.sh -h
+		bash "$7/m2m-coap.sh" get $5 $c 
+		sleep 3
+		############################# Stop Capture ###################################
+		pid=$(ps -e | pgrep tcpdump)  
+		#echo $pid  
+		#sleep 2
+		sudo kill -2 $pid
+		sleep 2
+		
+		echo " ################ End ##############"
+		
+		#################################### END ######################################
+		done
+		cd ..
+
+		;;
+
+		mqtt)
+		### MQTT ###
+		################### Run initial 1-X without loss #########################
+		echo "###### Initial phase start 1-$4 ###"
+		fol2="$fol-init"
+		mkdir $fol2
+		cd $fol2
+		############ Make sure it Remove NetEM #############
+		bash "$7/netem_ssh.sh" add
+		bash "$7/netem_ssh.sh" del 
+		# bash "$7/netem_ssh.sh" show
+		echo " Create folder in Subscriber side"
+		sshpass -p "howislife" ssh -t student@192.168.1.2 "cd TEST && mkdir $fol2-sub-init"
+		############### Start #################
+		for ((c=1; c<=$4;c++))
+		do
+			############## Start the process ##############
+			echo " ############# Round $c ###########"
+			############## Start Capture  #################
+			sudo tcpdump -i $1 -w $2-init-$c.pcap &
+			sleep 2
+			sname="~/TEST/$fol2-sub-$d/test-mqtt-$c"
+			####### Remote to another PC (Sub) and capture the packets ###########
+			
+			sshpass -p 'howislife' ssh student@192.168.1.2 "sudo tcpdump -i enp2s0 -w $sname 'port $3' " &
+			sleep 2
+
+			######## To received TCP ack might need to remove PORT ########
+			
+			sshpass -p 'howislife' ssh student@192.168.1.2  "bash ~/mqtt-sub-cap.sh enp2s0 '~/TEST/test-mqtt-$c' $3 $4 "& 
+
+			
+			bash "$7/m2m-mqtt.sh" mqtt_pub $4 $3 $6 $9
+			######## Wait for some left over packet ################
+			sleep 5
+
+			#echo $pid  
+			#sleep 2
+			############################# Stop Capture ###################################
+			sshpass -p 'howislife' ssh student@192.168.1.2 'sudo bash ~/mqtt-sub-cap.sh stop'
+
+			pid=$(ps -e | pgrep tcpdump) 
+			sleep 2
+			sudo kill -2 $pid
+
+			echo " ################ End ##############"
+
+
+		done
+		cd ..
+		#################################### END ######################################		
+		;;
+
+	esac
+	;;
+
 	*)
 
 	### CAPTURE ########
 	#Start#
+	#//////// Make sure it enable netem ////#
+	bash "$7/netem_ssh.sh" add 
+
+	#///////////////////////////////////////#
 	echo "Start"
 	fol=$2
 	mkdir $fol
 	cd $fol
 	#### Loss Loop #######
-	for ((d=0;d<=15;d++))
+	for ((d=0;d<=40;d=d+5))
 	do
 	
-	############# ADD LOSS ##################
-	echo "======== Loss $d % =============== "
-	if [ $(($d)) == 0 ]; then
-		bash "$7/netem_ssh.sh" 0.001 0.0
+	# ############# ADD LOSS ##################
+	# echo "======== Loss $d % =============== "
+	# if [ $d == 0 ]; then
+	# 	bash "$7/netem_ssh.sh" loss 0.001
 	
+	# else
+	# 	bash "$7/netem_ssh.sh" loss $d
+	# fi
+	# 	bash "$7/netem_ssh.sh" show
+	# echo "=================================="
+	# sleep 3
+	case $9 in
+		burst )
+	########### Optional for Burst loss ##########
+
+	echo "======== Loss $d % $d% =============== "
+	if [ $(($d)) == 0 ]; then
+		bash "$7/netem_ssh.sh" burst 0.001 0.001
+ 	
 	else
-	bash "$7/netem_ssh.sh" $(($d)) 0.0
+		bash "$7/netem_ssh.sh" burst $d $d 
 	fi
+	bash "$7/netem_ssh.sh" show
 	echo "=================================="
 	sleep 3
 
+	##############################################
+	########### Optional for delay ##########
+
+	# echo "======== delay $d ms =============== "
+	# 
+	# 	bash "$7/netem_ssh.sh" delay $d
+	#
+	# echo "=================================="
+	# sleep 3
+
+	##############################################
+	;;
+		loss )
+		echo "======== Loss $d % =============== "
+		if [ $d == 0 ]; then
+			bash "$7/netem_ssh.sh" loss 0.001
+		
+		else
+			bash "$7/netem_ssh.sh" loss $d
+		fi
+			bash "$7/netem_ssh.sh" show
+		echo "=================================="
+		sleep 3
+	esac
 	########### Optional for Burst loss ##########
 
 	# echo "======== Loss $d % $d% =============== "
 	# if [ $(($d)) == 0 ]; then
-	# 	bash "$7/netem_ssh.sh" 0.001 0.0 0.001
+	# 	bash "$7/netem_ssh.sh" burst 0.001 0.001
  	
 	# else
-	# 	bash "$7/netem_ssh.sh" $(($d)) 0.0 $(($d)) 
+	# 	bash "$7/netem_ssh.sh" burst $d $d 
 	# fi
 	# echo "=================================="
 	# sleep 3
 
 	##############################################
+	########### Optional for delay ##########
+
+	# echo "======== delay $d ms =============== "
+	# 
+	# 	bash "$7/netem_ssh.sh" delay $d
+	#
+	# echo "=================================="
+	# sleep 3
+
+	##############################################
+
 	case $8 in
 		coap )
 	
@@ -185,5 +349,6 @@ case $1 in
 	;;
 esac
 
+echo "################## See you, BYE #####################"
 
-######################################################### DONE ###############################################
+#################################################### DONE ###############################################
--- Scripts/iot-loop/netem_ssh.sh
+++ Scripts/iot-loop/netem_ssh.sh
@@ -1,11 +1,38 @@
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem loss $1%"
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem loss $1%"
-sshpass -p "IK2200ajpy"  ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem delay $2ms"
-sshpass -p "IK2200ajpy"  ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem delay $2ms"
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp2s0"
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp3s0"
-if [ $(($3)) != 0 ] ; then 
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem loss $1% $3%"
-sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem loss $1% $3%"
+# $1  Loss     $2  Delay   $3  Successive loss probablity 
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem loss $(($1))%"
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem loss $(($1))%"
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem delay $2ms"
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem delay $2ms"
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp2s0"
+# sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp3s0"
+case $1 in
+	loss )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem loss $2%"
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem loss $2%"
+	;;
 
-fi
+	burst )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem loss $2% $3%"
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem loss $2% $3%"
+	;;
+
+	delay )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp2s0 root netem delay $2ms"
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc change dev enp3s0 root netem delay $2ms"
+	;;
+	
+	add )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc add dev enp2s0 root netem loss 0.001%"
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc add dev enp3s0 root netem loss 0.001%"
+	;;
+
+	del )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc del dev enp2s0 root netem "
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc qdisc del dev enp3s0 root netem "
+	;;
+
+	show )
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp2s0"
+	sshpass -p "IK2200ajpy" ssh -t student@192.168.1.4 " sudo tc -s qdisc show dev enp3s0"
+	;;
+esac
